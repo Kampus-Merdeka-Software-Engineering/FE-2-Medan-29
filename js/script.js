2b2957da2d49690a712936f5ad8d6ce9e11bac3f
Munawarotul <afidohpml@gmail.com>
2023-11-30 13:04:08 +0700

    nambahin js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,20 @@
-// script.js
+//page berita agar bisa diklik tiap div
 document.addEventListener('DOMContentLoaded', function () {
-    // Get all elements with the class "article"
     var articles = document.querySelectorAll('.article');
 
-    // Add a click event listener to each article
     articles.forEach(function (article) {
         article.addEventListener('click', function () {
-            // Find the link inside the clicked article
             var link = this.querySelector('a');
-
-            // Check if the link exists
             if (link) {
-                // Get the link's href attribute
                 var linkUrl = link.getAttribute('href');
             }
         });
     });
 });
 
+//const BASE_URL = "link railway";
 
 // SUBMIT EMAIL MASIH REVISI
-
 function submitComment(event) {
     const email = document.querySelector("#email");
 
